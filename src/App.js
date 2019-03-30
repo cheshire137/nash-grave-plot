1541e7407df184a8706891dfeeea2d1c1ef55c71
Sarah Vessels <cheshire137@gmail.com>
2019-03-30 14:03:30 -0500

    Add a header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ class App extends Component {
   render() {
     return (
       <div>
+        <header>
+          <div className="container d-flex flex-items-center flex-justify-between">
+            <h1>NashGravePlot</h1>
+            <h2 className="f6 text-normal">
+              Data from <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://data.nashville.gov/Geneology/Davidson-County-Cemetery-Survey/ttqg-mpiz"
+              >Davidson County Cemetery Survey</a>
+            </h2>
+          </div>
+        </header>
         <IntermentList />
       </div>
     );
