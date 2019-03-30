cacbeac10aa5a075e8588b9c8a3933565fd21121
Sarah Vessels <cheshire137@gmail.com>
2019-03-30 09:32:03 -0500

    Use ReactTable to make columns sortable, paginate

--- src/components/IntermentList.js
+++ src/components/IntermentList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import Interment from '../models/Interment';
 
 class IntermentList extends Component {
@@ -15,14 +17,103 @@ class IntermentList extends Component {
     const { interments } = this.state;
 
     return (
-      <div>
-        {interments.slice(0, 10).map(interment => (
-          <div key={interment.key}>
-            Locale:
-            {interment.locale}
-          </div>
-        ))}
-      </div>
+      <ReactTable
+        data={interments}
+        columns={[
+          {
+            Header: "Person",
+            columns: [
+              {
+                Header: "Name",
+                accessor: "person"
+              },
+              {
+                Header: "Died",
+                accessor: "deathDate"
+              },
+              {
+                Header: "Info",
+                accessor: "deceasedInfo"
+              }
+            ]
+          },
+          {
+            Header: "Location",
+            columns: [
+              {
+                Header: "Cemetery",
+                accessor: "cemeteryName"
+              },
+              {
+                Header: "Address",
+                accessor: "address"
+              },
+              {
+                Header: "Graveyard Type",
+                accessor: "graveyardType"
+              },
+              {
+                Header: "Site History",
+                accessor: "siteHistory"
+              },
+              {
+                Header: "Notes",
+                accessor: "additionalLocationInfo"
+              }
+            ]
+          },
+          {
+            Header: "Marker/Plot",
+            columns: [
+              {
+                Header: "Inscription",
+                accessor: "inscription"
+              },
+              {
+                Header: "Footstone",
+                accessor: "footstone"
+              },
+              {
+                Header: "Demarcation",
+                accessor: "demarcation"
+              },
+              {
+                Header: "Condition",
+                accessor: "condition"
+              },
+              {
+                Header: "Accessible",
+                accessor: "accessible"
+              },
+              {
+                Header: "Restoration",
+                accessor: "restoration"
+              }
+            ]
+          },
+          {
+            Header: "Notes",
+            accessor: "notes"
+          },
+          {
+            Header: "Survey",
+            columns: [
+              {
+                Header: "Original",
+                accessor: "originalSurvey"
+              },
+              {
+                Header: "Updates",
+                accessor: "surveyUpdates"
+              },
+              {
+                Header: "Current",
+                accessor: "currentSurvey"
+              }
+            ]
+          }
+        ]}
+      />
     );
   }
 }
