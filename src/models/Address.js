090ffc3bed474faf0a0f5d1619722fd91a860bfd
Sarah Vessels <cheshire137@gmail.com>
2019-03-30 13:40:05 -0500

    Restore street numbers to display

--- src/models/Address.js
+++ src/models/Address.js
@@ -8,26 +8,26 @@ class Address {
       this.geocode = `${this.latitude}, ${this.longitude}`;
     }
     this.additionalLocationInfo = props['Additional Location Information'];
-    this.street = props.Street;
-    this.number = props.Number;
-    this.streetAddress = [props.Number, props.Street].
-      filter(info => typeof info === 'string' && info.trim().length > 0).
+    this.street = String(props.Street);
+    this.number = String(props.Number);
+    this.streetAddress = [this.number, this.street].
+      filter(info => info.trim().length > 0).
       map(info => info.trim()).
       join(' ');
   }
 
   toString() {
     const parts = [
-      this.locale,
-      this.mappedLocation,
-      this.latitude,
-      this.longitude,
-      this.additionalLocationInfo,
-      this.street,
-      this.number
+      String(this.locale),
+      String(this.mappedLocation),
+      String(this.latitude),
+      String(this.longitude),
+      String(this.additionalLocationInfo),
+      String(this.street),
+      String(this.number)
     ];
     return parts.
-      filter(part => typeof part === 'string' && part.trim().length > 0).
+      filter(part => part.trim().length > 0).
       join(' ');
   }
 }
