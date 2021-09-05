0b835a4916df60157413eef6588182238732b9a4
Sarah Vessels <cheshire137@gmail.com>
2021-09-05 18:11:57 -0500

    Use className not class attribute

--- src/components/AddressDisplay.js
+++ src/components/AddressDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddressDisplay = ({ streetAddress, additionalLocationInfo }) => {
   return (
-    <div class="constrained-text ws-normal">
+    <div className="constrained-text ws-normal">
       {streetAddress}
       {typeof additionalLocationInfo === 'string' && additionalLocationInfo.length > 0 ? (
         <div>{additionalLocationInfo}</div>
