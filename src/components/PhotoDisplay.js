8e786cbc9aef75e7c2a32acc17d23610a4986f42
Sarah Vessels <cheshire137@gmail.com>
2021-09-05 16:46:47 -0500

    Simplify PhotoDisplay

--- src/components/PhotoDisplay.js
+++ src/components/PhotoDisplay.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PhotoDisplay extends Component {
-  render() {
-    const { text, url, title } = this.props;
-
-    return (
-      <div>
-        {url && title ? (
-          <a
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >{title}</a>
-        ) : (
-          <span>{text}</span>
-        )}
-      </div>
-    );
-  }
-}
+const PhotoDisplay = ({ text, url, title }) => {
+  return (
+    <div>
+      {url && title ? (
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >{title}</a>
+      ) : (
+        <span>{text}</span>
+      )}
+    </div>
+  );
+};
 
 export default PhotoDisplay;
