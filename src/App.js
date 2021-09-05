bbe712d7a09ce0926c4a1ee32dfc8a6ae08570a8
Sarah Vessels <cheshire137@gmail.com>
2021-09-05 17:18:01 -0500

    Simplify App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import IntermentList from './components/IntermentList';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <div className="container d-flex flex-items-center flex-justify-between">
-            <h1>NashGravePlot</h1>
-            <h2 className="f6 text-normal">
-              Data from <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://data.nashville.gov/Geneology/Davidson-County-Cemetery-Survey/ttqg-mpiz"
-              >Davidson County Cemetery Survey</a>
-            </h2>
-          </div>
-        </header>
-        <IntermentList />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <header>
+        <div className="container d-flex flex-items-center flex-justify-between">
+          <h1>NashGravePlot</h1>
+          <h2 className="f6 text-normal">
+            Data from <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://data.nashville.gov/Geneology/Davidson-County-Cemetery-Survey/ttqg-mpiz"
+            >Davidson County Cemetery Survey</a>
+          </h2>
+        </div>
+      </header>
+      <IntermentList />
+    </>
+  );
+};
 
 export default App;
