b7e688974ceedffeead03a6918c905da17f733c6
Sarah Vessels <cheshire137@gmail.com>
2021-09-05 19:23:53 -0500

    Fix address filtering error

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,13 +1,15 @@
 class Address {
   constructor(props) {
-    this.locale = props.locale;
-    this.mappedLocation = props.mapped_location;
+    this.locale = props.locale || '';
+    this.mappedLocation = props.mapped_location || '';
     this.latitude = props.latitude;
     this.longitude = props.longitude;
     if (this.latitude && this.longitude) {
       this.geocode = `${this.latitude}, ${this.longitude}`;
+    } else {
+      this.geocode = '';
     }
-    this.additionalLocationInfo = props.additional_location_information;
+    this.additionalLocationInfo = props.additional_location_information || '';
     this.street = props.street || '';
     this.number = props.number || '';
     this.streetAddress = [this.number, this.street].filter(info => info.trim().length > 0)
@@ -15,15 +17,7 @@ class Address {
   }
 
   toString() {
-    const parts = [
-      this.locale || '',
-      this.mappedLocation || '',
-      this.latitude || '',
-      this.longitude || '',
-      this.additionalLocationInfo || '',
-      this.street || '',
-      this.number || ''
-    ];
+    const parts = [this.locale, this.geocode, this.additionalLocationInfo, this.streetAddress];
     return parts.filter(part => part.trim().length > 0).join(' ');
   }
 }
