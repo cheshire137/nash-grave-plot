46f1ca6f058502c5b94226a668d3acea11853856
Sarah Vessels <cheshire137@gmail.com>
2021-09-11 17:57:05 -0500

    Fix condition width

--- src/components/IntermentList.js
+++ src/components/IntermentList.js
@@ -103,7 +103,7 @@ const IntermentList = () => {
           {
             Header: 'Condition',
             accessor: 'condition',
-            minWidth: 140
+            Cell: DemarcationDisplay
           },
           {
             Header: 'Accessible',
