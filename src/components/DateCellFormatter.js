f23fad3c0feb10708887cbc3acebc2514768b05d
Sarah Vessels <cheshire137@gmail.com>
2021-09-11 17:05:00 -0500

    Don't wrap dates

--- src/components/DateCellFormatter.js
+++ src/components/DateCellFormatter.js
@@ -1,17 +1,24 @@
-const DateCellFormatter = ({value}) => {
-  if (value instanceof Date) {
-    const year = value.getFullYear();
-    let month = value.getMonth() + 1;
-    if (month < 10) {
-      month = `0${month}`;
-    }
-    let day = value.getDate();
-    if (day < 10) {
-      day = `0${day}`;
-    }
-    return `${year}-${month}-${day}`;
-  }
+import React from 'react';
+import {Text} from '@primer/components';
 
+const prettyDateStr = (date) => {
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  let day = date.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  return `${year}-${month}-${day}`;
+};
+
+const DateCellFormatter = ({ value }) => {
+  if (value instanceof Date) {
+    const displayValue = prettyDateStr(value);
+    return <Text whiteSpace="nowrap">{displayValue}</Text>;
+  }
   return value;
 };
 
