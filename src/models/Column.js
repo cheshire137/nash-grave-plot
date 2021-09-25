ee41ca1b4d99ee0f60e12226542c6b36bf4a4102
Sarah Vessels <cheshire137@gmail.com>
2021-09-25 13:03:19 -0500

    Add Column model

---
+++ src/models/Column.js
@@ -0,0 +1,27 @@
+class Column {
+  static allColumns = [
+    'person',
+    'deathDate',
+    'deceasedInfo',
+    'cemeteryName',
+    'address',
+    'graveyardType',
+    'siteHistory',
+    'inscription',
+    'footstone',
+    'demarcation',
+    'condition',
+    'accessible',
+    'restoration',
+    'gravePhotos',
+    'notes',
+    'tractParcelNumber',
+    'cemeteryParcelNumber',
+    'originalSurvey',
+    'surveyUpdates',
+    'currentSurvey'
+  ];
+  static defaultColumns = Column.allColumns;
+}
+
+export default Column;
