42ec1b00a0f5b7d4c2cad50c764c8d377811062f
Sarah Vessels <cheshire137@gmail.com>
2021-09-25 13:42:24 -0500

    Pass enabled state and toggler from group opts

--- src/components/ColumnGroupOptions.js
+++ src/components/ColumnGroupOptions.js
@@ -3,7 +3,7 @@ import { Box, Heading } from '@primer/components';
 import ColumnOption from './ColumnOption';
 import Column from '../models/Column';
 
-const ColumnGroupOptions = ({ groupName, columnValues }) => {
+const ColumnGroupOptions = ({ groupName, enabledColumns, columnValues, onColumnToggle }) => {
   return (
     <Box mb={4}>
       {groupName && groupName.trim().length > 0 && <Heading fontSize={2} mb={2}>{groupName}</Heading>}
@@ -11,7 +11,8 @@ const ColumnGroupOptions = ({ groupName, columnValues }) => {
         key={"${groupName}-#{value}"}
         name={Column.names[value]}
         value={value}
-        onToggle={isEnabled => console.log(value, 'is', isEnabled ? 'enabled' : 'disabled')}
+        isEnabled={enabledColumns.includes(value)}
+        onToggle={onColumnToggle}
       />)}
     </Box>
   );
