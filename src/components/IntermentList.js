dcbfa7ac25945597e44d3bcb0fef817190d5f0f2
Sarah Vessels <cheshire137@gmail.com>
2021-09-25 12:50:38 -0500

    Pull out column and column group vars

--- src/components/IntermentList.js
+++ src/components/IntermentList.js
@@ -27,141 +27,167 @@ const IntermentList = () => {
   }, []);
   const defaultColumn = useMemo(() => ({ Filter: TextFilter }), []);
 
+  const nameColumn = {
+    Header: 'Name',
+    accessor: 'person',
+    filter: 'fuzzyText',
+    Cell: NameDisplay
+  };
+  const deathDateColumn = {
+    Header: 'Died',
+    accessor: 'deathDate',
+    Cell: DiedDateDisplay
+  };
+  const deceasedInfoColumn = {
+    Header: 'Info',
+    accessor: 'deceasedInfo',
+    Cell: InfoDisplay,
+    filter: 'fuzzyText'
+  };
+  const personColumnGroup = {
+    Header: 'Person',
+    columns: [
+      nameColumn,
+      deathDateColumn,
+      deceasedInfoColumn
+    ]
+  };
+  const cemeteryColumn = {
+    Header: 'Cemetery',
+    accessor: 'cemeteryName',
+    filter: 'includes',
+    Filter: SelectColumnFilter,
+    Cell: NameDisplay
+  };
+  const addressColumn = {
+    Header: 'Address',
+    accessor: 'address',
+    Cell: AddressDisplay,
+    filter: 'fuzzyText'
+  };
+  const graveyardTypeColumn = {
+    Header: 'Graveyard Type',
+    accessor: 'graveyardType',
+    filter: 'includes',
+    Filter: SelectColumnFilter,
+    Cell: GraveyardTypeDisplay
+  };
+  const siteHistoryColumn = {
+    Header: 'Site History',
+    accessor: 'siteHistory',
+    Cell: InfoDisplay
+  };
+  const locationColumnGroup = {
+    Header: 'Location',
+    columns: [
+      cemeteryColumn,
+      addressColumn,
+      graveyardTypeColumn,
+      siteHistoryColumn
+    ]
+  };
+  const inscriptionColumn = {
+    Header: 'Inscription',
+    accessor: 'inscription',
+    Cell: InscriptionDisplay
+  };
+  const footstoneColumn = {
+    Header: 'Footstone',
+    accessor: 'footstone',
+    Cell: FootstoneDisplay
+  };
+  const demarcationColumn = {
+    Header: 'Demarcation',
+    accessor: 'demarcation',
+    Cell: DemarcationDisplay
+  };
+  const conditionColumn = {
+    Header: 'Condition',
+    accessor: 'condition',
+    Cell: DemarcationDisplay
+  };
+  const accessibleColumn = {
+    Header: 'Accessible',
+    accessor: 'accessible',
+    filter: 'includes',
+    Filter: SelectColumnFilter
+  };
+  const restorationColumn = {
+    Header: 'Restoration',
+    accessor: 'restoration',
+    Cell: LongTextBlock
+  };
+  const photosColumn = {
+    Header: 'Photos',
+    accessor: 'gravePhotos',
+    Cell: PhotoList
+  };
+  const markerColumnGroup = {
+    Header: 'Marker/Plot',
+    columns: [
+      inscriptionColumn,
+      footstoneColumn,
+      demarcationColumn,
+      conditionColumn,
+      accessibleColumn,
+      restorationColumn,
+      photosColumn
+    ]
+  };
+  const notesColumn = {
+    Header: 'Notes',
+    accessor: 'notes',
+    Cell: NotesDisplay
+  };
+  const tractParcelNumberColumn = {
+    Header: 'Tract',
+    accessor: 'tractParcelNumber',
+    Cell: ParcelNumberDisplay
+  };
+  const cemeteryParcelNumberColumn = {
+    Header: 'Cemetery',
+    accessor: 'cemeteryParcelNumber',
+    Cell: ParcelNumberDisplay
+  };
+  const parcelNumberColumnGroup = {
+    Header: 'Parcel Numbers',
+    columns: [
+      tractParcelNumberColumn,
+      cemeteryParcelNumberColumn
+    ]
+  };
+  const originalSurveyColumn = {
+    Header: 'Original',
+    accessor: 'originalSurvey',
+    Cell: DateCellFormatter
+  };
+  const surveyUpdatesColumn = {
+    Header: 'Updates',
+    accessor: 'surveyUpdates',
+    Cell: DateCellFormatter
+  };
+  const currentSurveyColumn = {
+    Header: 'Current',
+    accessor: 'currentSurvey',
+    Cell: DateCellFormatter
+  };
+  const surveyColumnGroup = {
+    Header: 'Survey',
+    columns: [
+      originalSurveyColumn,
+      surveyUpdatesColumn,
+      currentSurveyColumn
+    ]
+  };
+
   const columns = useMemo(() => {
     return [
-      {
-        Header: 'Person',
-        columns: [
-          {
-            Header: 'Name',
-            accessor: 'person',
-            filter: 'fuzzyText',
-            Cell: NameDisplay
-          },
-          {
-            Header: 'Died',
-            accessor: 'deathDate',
-            Cell: DiedDateDisplay
-          },
-          {
-            Header: 'Info',
-            accessor: 'deceasedInfo',
-            Cell: InfoDisplay,
-            filter: 'fuzzyText'
-          }
-        ]
-      },
-      {
-        Header: 'Location',
-        columns: [
-          {
-            Header: 'Cemetery',
-            accessor: 'cemeteryName',
-            filter: 'includes',
-            Filter: SelectColumnFilter,
-            Cell: NameDisplay
-          },
-          {
-            Header: 'Address',
-            accessor: 'address',
-            Cell: AddressDisplay,
-            filter: 'fuzzyText'
-          },
-          {
-            Header: 'Graveyard Type',
-            accessor: 'graveyardType',
-            filter: 'includes',
-            Filter: SelectColumnFilter,
-            Cell: GraveyardTypeDisplay
-          },
-          {
-            Header: 'Site History',
-            accessor: 'siteHistory',
-            Cell: InfoDisplay
-          }
-        ]
-      },
-      {
-        Header: 'Marker/Plot',
-        columns: [
-          {
-            Header: 'Inscription',
-            accessor: 'inscription',
-            Cell: InscriptionDisplay
-          },
-          {
-            Header: 'Footstone',
-            accessor: 'footstone',
-            Cell: FootstoneDisplay
-          },
-          {
-            Header: 'Demarcation',
-            accessor: 'demarcation',
-            Cell: DemarcationDisplay
-          },
-          {
-            Header: 'Condition',
-            accessor: 'condition',
-            Cell: DemarcationDisplay
-          },
-          {
-            Header: 'Accessible',
-            accessor: 'accessible',
-            filter: 'includes',
-            Filter: SelectColumnFilter
-          },
-          {
-            Header: 'Restoration',
-            accessor: 'restoration',
-            Cell: LongTextBlock
-          },
-          {
-            Header: 'Photos',
-            accessor: 'gravePhotos',
-            Cell: PhotoList
-          }
-        ]
-      },
-      {
-        Header: 'Notes',
-        accessor: 'notes',
-        Cell: NotesDisplay
-      },
-      {
-        Header: 'Parcel Numbers',
-        columns: [
-          {
-            Header: 'Tract',
-            accessor: 'tractParcelNumber',
-            Cell: ParcelNumberDisplay
-          },
-          {
-            Header: 'Cemetery',
-            accessor: 'cemeteryParcelNumber',
-            Cell: ParcelNumberDisplay
-          }
-        ]
-      },
-      {
-        Header: 'Survey',
-        columns: [
-          {
-            Header: 'Original',
-            accessor: 'originalSurvey',
-            Cell: DateCellFormatter
-          },
-          {
-            Header: 'Updates',
-            accessor: 'surveyUpdates',
-            Cell: DateCellFormatter
-          },
-          {
-            Header: 'Current',
-            accessor: 'currentSurvey',
-            Cell: DateCellFormatter
-          }
-        ]
-      }
+      personColumnGroup,
+      locationColumnGroup,
+      markerColumnGroup,
+      notesColumn,
+      parcelNumberColumnGroup,
+      surveyColumnGroup
     ];
   }, []);
 
