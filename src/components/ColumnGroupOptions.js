4f2d89b2f12dbd6d3a5e50049a3a71fa9f61a941
Sarah Vessels <cheshire137@gmail.com>
2021-09-25 13:44:51 -0500

    Wow what is this string interpolation
    quasi ruby looking mfer

--- src/components/ColumnGroupOptions.js
+++ src/components/ColumnGroupOptions.js
@@ -8,7 +8,7 @@ const ColumnGroupOptions = ({ groupName, enabledColumns, columnValues, onColumnT
     <Box mb={4}>
       {groupName && groupName.trim().length > 0 && <Heading fontSize={2} mb={2}>{groupName}</Heading>}
       {columnValues.map(value => <ColumnOption
-        key={"${groupName}-#{value}"}
+        key={`${groupName}-${value}`}
         name={Column.names[value]}
         value={value}
         isEnabled={enabledColumns.includes(value)}
