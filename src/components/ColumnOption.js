8d1a1b795c56167ae6e0a4671d30ee539a60348e
Sarah Vessels <cheshire137@gmail.com>
2021-09-25 13:40:28 -0500

    Set ID attr on checkbox and 'for' on label

--- src/components/ColumnOption.js
+++ src/components/ColumnOption.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Box, FormGroup } from '@primer/components';
 
 const ColumnOption = ({ name, value, onToggle }) => {
+  const domID = `enabledColumns_${value}`;
   return (
     <FormGroup display="inline-block" my={0} mr={3}>
-      <FormGroup.Label fontWeight="normal">
+      <FormGroup.Label fontWeight="normal" htmlFor={domID}>
         <input
           type="checkbox"
           value={value}
           onChange={e => onToggle(e.target.checked)}
+          id={domID}
         />
         <Box as="span" display="inline-block" ml={1}>{name}</Box>
       </FormGroup.Label>
