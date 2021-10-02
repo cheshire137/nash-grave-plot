c36b69b94f6a1ad239ab6b4f944d4abc621e1b3c
Sarah Vessels <cheshire137@gmail.com>
2021-10-02 16:34:25 -0500

    Drop debounce, pull out UpdateUrlFilter

--- src/components/AddressFilter.js
+++ src/components/AddressFilter.js
@@ -1,28 +1,12 @@
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 import TextFilter from './TextFilter';
-import debounce from 'lodash.debounce';
+import UpdateUrlFilter from '../models/UpdateUrlFilter';
 
 function AddressFilter({
   column: { filterValue, setFilter }
 }) {
-  const updateUrl = () => {
-    if (!filterValue || filterValue.trim().length < 1) {
-      if (window.location.search !== '') {
-        window.history.pushState({}, '', '');
-      }
-      return;
-    }
-    if (window.history.pushState) {
-      const newUrlSearch = `?address=${encodeURIComponent(filterValue)}`;
-      if (window.location.search !== newUrlSearch) {
-        window.history.pushState({}, '', newUrlSearch);
-      }
-    }
-  };
-  const debouncedUpdateUrl = useMemo(() => debounce(updateUrl, 300), [filterValue]);
-  useEffect(() => { return () => debouncedUpdateUrl.cancel(); }, []);
   const setFilterAndUpdateUrl = newAddress => {
-    debouncedUpdateUrl();
+    UpdateUrlFilter('address', newAddress);
     setFilter(newAddress);
   }
   return <TextFilter column={{ filterValue, setFilter: setFilterAndUpdateUrl }} />;
---
+++ src/models/UpdateUrlFilter.js
@@ -0,0 +1,12 @@
+function UpdateUrlFilter(key, value) {
+  if (window.history.pushState) {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+    const newUrlSearch = searchParams.toString();
+    if (window.location.search !== newUrlSearch) {
+      window.history.pushState({}, '', newUrlSearch);
+    }
+  }
+}
+
+export default UpdateUrlFilter;
