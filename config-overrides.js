bc58b1e41d34ecf0e202ae987628a037a98e5fad
Sarah Vessels <cheshire137@gmail.com>
2022-12-17 17:07:37 -0600

    Start converting to TypeScript

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+module.exports = function override(config) {
+  config.resolve.fallback = Object.assign(config.resolve.fallback || {}, {
+    // "buffer": require.resolve("buffer"),
+    // "timers": require.resolve("timers-browserify"),
+    // "crypto": require.resolve("crypto-browserify"),
+    // "stream": require.resolve("stream-browserify"),
+    // "assert": require.resolve("assert"),
+    // "http": require.resolve("stream-http"),
+    // "https": require.resolve("https-browserify"),
+    // "os": require.resolve("os-browserify"),
+    // "url": require.resolve("url")
+  });
+  // config.plugins = (config.plugins || []).concat([
+  //   new webpack.ProvidePlugin({
+  //     process: 'process/browser',
+  //     Buffer: ['buffer', 'Buffer']
+  //   })
+  // ])
+  return config;
+}
