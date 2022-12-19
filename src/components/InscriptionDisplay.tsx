f093004786440dde392a3849f58328d689003701
Sarah Vessels <cheshire137@gmail.com>
2022-12-19 14:30:06 -0600

    Pull out InscriptionLines

--- src/components/InscriptionDisplay.tsx
+++ src/components/InscriptionDisplay.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
 import { Box } from '@primer/react';
-import { titleCase } from './TitleCase';
 import type { CellProps } from 'react-table';
-import ConstrainedTextBlock from './ConstrainedTextBlock';
-
-
-interface InscriptionLinesProps {
-  text: string;
-  lines: string[];
-}
-
-const InscriptionLines = ({ text, lines }: InscriptionLinesProps) => <ConstrainedTextBlock textAlign="center">
-  {lines.length > 0 ? (
-    <>
-      {lines.map(line => {
-        const key = '_' + Math.random().toString(36).substr(2, 9);
-        return (
-          <div key={key}>{titleCase(line)}</div>
-        );
-      })}
-    </>
-  ) : text}
-</ConstrainedTextBlock>;
+import InscriptionLines from './InscriptionLines';
 
 const InscriptionDisplay = ({ value }: CellProps<Record<string, unknown>>) => <Box minWidth="200px">
   <InscriptionLines {...value} />
---
+++ src/components/InscriptionLines.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { titleCase } from './TitleCase';
+import ConstrainedTextBlock from './ConstrainedTextBlock';
+
+interface Props {
+  text: string;
+  lines: string[];
+}
+
+const InscriptionLines = ({ text, lines }: Props) => <ConstrainedTextBlock textAlign="center">
+  {lines.length > 0 ? (
+    <>
+      {lines.map(line => {
+        const key = '_' + Math.random().toString(36).substr(2, 9);
+        return (
+          <div key={key}>{titleCase(line)}</div>
+        );
+      })}
+    </>
+  ) : text}
+</ConstrainedTextBlock>;
+
+export default InscriptionLines;
