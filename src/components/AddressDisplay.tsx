3771e45d3ff5cbb23665417f7690bdaea7ae2d1c
Sarah Vessels <cheshire137@gmail.com>
2022-12-19 14:28:41 -0600

    Pull out AddressLines

--- src/components/AddressDisplay.tsx
+++ src/components/AddressDisplay.tsx
@@ -1,21 +1,6 @@
 import React from 'react';
-import { Box } from '@primer/react';
-import TitleCase from './TitleCase';
 import type { CellProps } from 'react-table';
-
-interface AddressLinesProps {
-  streetAddress: string;
-  additionalLocationInfo?: string;
-}
-
-const AddressLines = ({ streetAddress, additionalLocationInfo }: AddressLinesProps) => <Box textAlign="left" minWidth="200px">
-  <TitleCase value={streetAddress} />
-  {typeof additionalLocationInfo === 'string' && additionalLocationInfo.length > 0 ? (
-    <Box>
-      <TitleCase value={additionalLocationInfo} />
-    </Box>
-  ) : null}
-</Box>;
+import AddressLines from './AddressLines';
 
 const AddressDisplay = ({ value }: CellProps<Record<string, unknown>>) => <AddressLines {...value} />;
 
---
+++ src/components/AddressLines.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box } from '@primer/react';
+import TitleCase from './TitleCase';
+
+interface Props {
+  streetAddress: string;
+  additionalLocationInfo?: string;
+}
+
+const AddressLines = ({ streetAddress, additionalLocationInfo }: Props) => <Box textAlign="left" minWidth="200px">
+  <TitleCase value={streetAddress} />
+  {typeof additionalLocationInfo === 'string' && additionalLocationInfo.length > 0 ? (
+    <Box>
+      <TitleCase value={additionalLocationInfo} />
+    </Box>
+  ) : null}
+</Box>;
+
+export default AddressLines;
