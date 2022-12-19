80fd6bbd34ae57c32d09b7c9f17ef22ec167532f
Sarah Vessels <cheshire137@gmail.com>
2022-12-19 14:48:49 -0600

    Use Primer Link for photos

--- src/components/PhotoDisplay.tsx
+++ src/components/PhotoDisplay.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import type PhotoLink from '../types/PhotoLink';
+import { Link } from '@primer/react';
 
 const PhotoDisplay = ({ url, description }: PhotoLink) => <div>
-  {url && description ? <a
+  {url && description ? <Link
     href={url}
     target="_blank"
     rel="noopener noreferrer"
-  >{description}</a> : <span>{url}</span>}
+  >{description}</Link> : <span>{url}</span>}
 </div>;
 
 export default PhotoDisplay;
