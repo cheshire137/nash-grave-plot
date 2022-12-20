4c842378fcb06b2799c97765b49742075c0f2d46
Sarah Vessels <cheshire137@gmail.com>
2022-12-20 16:00:01 -0600

    Mention data age in footer
    Based on update date currently shown on https://data.nashville.gov/Genealogy/Davidson-County-Cemetery-Survey/ttqg-mpiz from when I downloaded the JSON a few days ago.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,21 +11,25 @@ const Footer = () => <Box
   display="flex"
   alignItems="center"
   justifyContent="center"
+  sx={{ fontSize: 1 }}
 >
   <Link
     target="_blank"
-    sx={{ display: 'inline-block', mr: 4 }}
+    sx={{ display: 'inline-block' }}
     muted={true}
     rel="noopener noreferrer"
     href="https://github.com/cheshire137/nash-grave-plot"
   >View source</Link>
-  <span>
+  <Box display="inline-block" mx="4">
     Data from <Link
       target="_blank"
       muted={true}
       rel="noopener noreferrer"
       href="https://data.nashville.gov/Geneology/Davidson-County-Cemetery-Survey/ttqg-mpiz"
     >Davidson County Cemetery Survey</Link>
+  </Box>
+  <span>
+    Data last updated Dec 12, 2022
   </span>
 </Box>
 
