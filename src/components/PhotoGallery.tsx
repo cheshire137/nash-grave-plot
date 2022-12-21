d264e6428b17cb03d2e71bdfb223f247fb97958c
Sarah Vessels <cheshire137@gmail.com>
2022-12-21 17:25:59 -0600

    Show a few photos on the gallery page

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,15 +1,26 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import { PageContext } from '../contexts/PageContext';
 import { CemeteryDataContext } from '../contexts/CemeteryDataContext';
+import { Gallery, Image } from 'react-grid-gallery';
 
 const PhotoGallery = () => {
   const { interments } = useContext(CemeteryDataContext);
   const { setPageTitle } = useContext(PageContext);
+  const imageData: Image[] = useMemo(() => {
+    const intermentsWithPhotos = interments.filter(interment => interment.hasPhotos());
+    let photoCaptionsByUrl: { [url: string]: string } = {};
+    for (const interment of intermentsWithPhotos) {
+      photoCaptionsByUrl = Object.assign(photoCaptionsByUrl, interment.getPhotoCaptionsByUrl());
+    }
+    return Object.keys(photoCaptionsByUrl).map(src => {
+      const caption = photoCaptionsByUrl[src];
+      return { src, caption, alt: caption, width: 200, height: 200 };
+    });
+  }, [interments]);
 
   useEffect(() => setPageTitle('Photo gallery'), [setPageTitle]);
 
-  return <>
-  </>;
+  return <Gallery enableImageSelection={false} images={imageData.slice(0, 10)} />;
 };
 
 export default PhotoGallery;
