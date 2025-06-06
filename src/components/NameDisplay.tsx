5a18b65e6d3811c44c7f1468a926eff0503e1fdd
Sarah Vessels <cheshire137@gmail.com>
2025-06-06 15:29:18 -0500

    CSS module for NameDisplay, no default export

--- src/components/NameDisplay.tsx
+++ src/components/NameDisplay.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import {Box} from '@primer/react'
 import TitleCase from './TitleCase'
 import type {CellProps} from 'react-table'
 import Person from '../models/Person'
 import DateCellFormatter from './DateCellFormatter'
+import styles from './NameDisplay.module.css'
 
-const NameDisplay = ({value}: CellProps<Record<string, unknown>>) => {
+export function NameDisplay({value}: CellProps<Record<string, unknown>>) {
   const person = value as Person
   return (
-    <Box minWidth="200px">
+    <div className={styles.container}>
       <TitleCase value={person.name} />
       <DateCellFormatter value={person.deathDate} />
-    </Box>
+    </div>
   )
 }
-
-export default NameDisplay
