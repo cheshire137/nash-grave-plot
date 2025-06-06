89133fe15f4f96f1a9e6b9eccdd71099460cd6da
Sarah Vessels <cheshire137@gmail.com>
2025-06-06 14:23:31 -0500

    Improve SettingsDialog appearance

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -1,5 +1,5 @@
-import React, {useRef, useState} from 'react'
-import {Box, Dialog, Text} from '@primer/react'
+import {useRef, useState} from 'react'
+import {Dialog} from '@primer/react'
 import {GearIcon} from '@primer/octicons-react'
 import IntermentFieldGroupSettings from './IntermentFieldGroupSettings'
 import LocalStorage from '../models/LocalStorage'
@@ -7,6 +7,7 @@ import {allIntermentFieldGroups} from '../constants'
 import type {IntermentField} from '../types'
 import {getEnabledColumns} from '../utils'
 import HeaderIconButton from './HeaderIconButton'
+import styles from './SettingsDialog.module.css'
 
 interface SettingsDialogProps {
   enabledFields: IntermentField[]
@@ -20,27 +21,20 @@ export function SettingsDialog({enabledFields, setEnabledFields}: SettingsDialog
     <>
       <HeaderIconButton ref={buttonRef} icon={GearIcon} onClick={() => setIsOpen(true)} aria-label="Settings" />
       {isOpen && (
-        <Dialog returnFocusRef={buttonRef} onClose={() => setIsOpen(false)} aria-labelledby="header-id">
-          <Dialog.Header id="header-id">Settings</Dialog.Header>
-          <Box p={3} sx={{overflow: 'auto', maxHeight: '70vh'}}>
-            <Text color="black" as="div">
-              <Box mt={0} mb={3} as="p">
-                Choose which columns to show:
-              </Box>
-              {allIntermentFieldGroups.map((group) => (
-                <IntermentFieldGroupSettings
-                  key={group}
-                  group={group}
-                  enabledFields={enabledFields}
-                  toggleFieldEnabled={(intermentField, isEnabled) => {
-                    const newEnabledFields = getEnabledColumns(enabledFields, intermentField, isEnabled)
-                    LocalStorage.set('enabledFields', newEnabledFields)
-                    setEnabledFields(newEnabledFields)
-                  }}
-                />
-              ))}
-            </Text>
-          </Box>
+        <Dialog title="Settings" returnFocusRef={buttonRef} onClose={() => setIsOpen(false)}>
+          <p className={styles.instructions}>Choose which columns to show:</p>
+          {allIntermentFieldGroups.map((group) => (
+            <IntermentFieldGroupSettings
+              key={group}
+              group={group}
+              enabledFields={enabledFields}
+              toggleFieldEnabled={(intermentField, isEnabled) => {
+                const newEnabledFields = getEnabledColumns(enabledFields, intermentField, isEnabled)
+                LocalStorage.set('enabledFields', newEnabledFields)
+                setEnabledFields(newEnabledFields)
+              }}
+            />
+          ))}
         </Dialog>
       )}
     </>
