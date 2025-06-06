8acf0478e90847096f3597be0e9d8ff788a1f513
Sarah Vessels <cheshire137@gmail.com>
2025-06-06 15:30:21 -0500

    CSS module for NotesDisplay, no default export

--- src/components/NotesDisplay.tsx
+++ src/components/NotesDisplay.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import {Box} from '@primer/react'
 import LongTextBlock from './LongTextBlock'
 import type {CellProps} from 'react-table'
+import styles from './NotesDisplay.module.css'
 
-const NotesDisplay = (props: CellProps<Record<string, unknown>>) => (
-  <Box minWidth="300px" textAlign="left">
-    <LongTextBlock {...props} />
-  </Box>
-)
-
-export default NotesDisplay
+export function NotesDisplay(props: CellProps<Record<string, unknown>>) {
+  return (
+    <div className={styles.container}>
+      <LongTextBlock {...props} />
+    </div>
+  )
+}
