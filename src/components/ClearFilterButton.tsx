0adf8dac3c62b2a3150b67264324c250d7d873fb
Sarah Vessels <cheshire137@gmail.com>
2025-06-06 13:04:06 -0500

    No default ClearFilterButton export

--- src/components/ClearFilterButton.tsx
+++ src/components/ClearFilterButton.tsx
@@ -1,15 +1,15 @@
 import {IconButton, type ButtonBaseProps} from '@primer/react'
 import {XIcon} from '@primer/octicons-react'
 
-const ClearFilterButton = (props: Omit<ButtonBaseProps, 'aria-labelledby'>) => (
-  <IconButton
-    size="small"
-    variant="invisible"
-    aria-label="Clear filter"
-    title="Clear filter"
-    icon={XIcon}
-    {...props}
-  />
-)
-
-export default ClearFilterButton
+export function ClearFilterButton(props: Omit<ButtonBaseProps, 'aria-labelledby'>) {
+  return (
+    <IconButton
+      size="small"
+      variant="invisible"
+      aria-label="Clear filter"
+      title="Clear filter"
+      icon={XIcon}
+      {...props}
+    />
+  )
+}
