935c2b15cc97220df5656959365bdf077e92e47a
Sarah Vessels <cheshire137@gmail.com>
2025-06-07 10:02:47 -0500

    Fix returnFocusRef setup in FilterDialog

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,16 +1,19 @@
+import type {RefObject} from 'react'
 import {IconButton, type IconButtonProps} from '@primer/react'
 import {FilterIcon} from '@primer/octicons-react'
 
 interface FilterButtonProps
   extends Omit<IconButtonProps, 'aria-labelledby' | 'aria-label' | 'icon' | 'size' | 'title' | 'variant'> {
   isOpen?: boolean
+  ref?: RefObject<HTMLButtonElement> | null
   title?: string
 }
 
-export function FilterButton({isOpen = false, title = 'Change filter', ...rest}: FilterButtonProps) {
+export function FilterButton({isOpen = false, ref, title = 'Change filter', ...rest}: FilterButtonProps) {
   return (
     <IconButton
       size="small"
+      ref={ref}
       aria-label={title}
       variant={isOpen ? 'default' : 'invisible'}
       icon={FilterIcon}
